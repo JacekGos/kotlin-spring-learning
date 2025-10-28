84b1268ca60b4b65ba866756a2a184b5cdf01813
JacekGos <jacekg93@gmail.com>
2025-10-28 08:17:45 +0100

    16.

--- src/main/kotlin/com/kotlin/playground/functions/Functions.kt
+++ src/main/kotlin/com/kotlin/playground/functions/Functions.kt
@@ -1,11 +1,18 @@
 package com.kotlin.playground.functions
 
+import java.time.LocalDate
+
 fun main() {
 
     printName("Jack")
+    printPersonDetails("Jacek", dob = LocalDate.parse("2025-10-26"))
 }
 
-fun printName(name: String) : Unit {
+fun printPersonDetails(name: String, email: String = "dupsko", dob: LocalDate = LocalDate.now()) {
+    println("name: $name, email: $email, dob: $dob")
+}
+
+fun printName(name: String): Unit {
     println("name is $name")
     val result = addition(1, 2)
     println("add result $result")
@@ -18,4 +25,6 @@ fun addition(x: Int, y: Int): Int {
     return x + y
 }
 
-fun additionExpression(x: Int, y: Int) : Int =  x + y
+fun additionExpression(x: Int, y: Int): Int = x + y
+
+
