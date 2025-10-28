9f894fd3fbf2973ee4accc1d1c4d949356692453
JacekGos <jacekg93@gmail.com>
2025-10-28 17:17:50 +0100

    22-2.

--- src/main/kotlin/com/kotlin/playground/classes/Item.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Item.kt
@@ -1,8 +1,23 @@
 package com.kotlin.playground.classes
 
+import java.lang.IllegalArgumentException
+
 class Item() {
 
     var name: String = ""
+        get() {
+            println("name getter")
+            return field
+        }
+    var price: Double = 0.0
+        get() = field
+        set(value) {
+            if (value >= 0.0) {
+                field = value
+            } else {
+                throw IllegalArgumentException("Negative price is not Allowed")
+            }
+        }
 
     constructor(name: String) : this() {
         this.name = name
@@ -13,4 +28,8 @@ fun main() {
 
     val item = Item(name = "Phone")
     println("Item name is ${item.name}")
+    println("Item price is ${item.price}")
+
+    item.price = 1.0
+    println("Item price after update is ${item.price}")
 }
