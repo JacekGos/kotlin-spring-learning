ccc93d3edb2e359486d754cbf4a1d27ce8285d9f
JacekGos <jacekg93@gmail.com>
2025-10-29 08:46:54 +0100

    33.

--- src/main/kotlin/com/kotlin/playground/classes/Course.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Course.kt
@@ -3,22 +3,33 @@ package com.kotlin.playground.classes
 data class Course(
     val id: Int,
     var name: String,
-    val author: String
+    val author: String,
+    val courseCategory: CourseCategory = CourseCategory.DEVELOPMENT
 ) {
+}
+
+enum class CourseCategory {
+    DEVELOPMENT,
+    BUSINESS,
+    DESIGN,
+    MARKETING
 }
 
 
 fun main() {
     val course = Course(1, "Kotlin tutorial", "Someone")
+    println(course)
 
 
-    val course1 = Course(1, "Kotlin tutorial", "Someone")
+    val course1 = Course(1, "Kotlin tutorial", "Someone", CourseCategory.DESIGN)
+    println(course1)
 
     println("Cheking equality: ${course == course1}")
 
     val course3 = course1.copy(
         id = 3,
-        author = "Some other"
+        author = "Some other",
+        courseCategory = CourseCategory.BUSINESS
     )
 
     println(course3)
