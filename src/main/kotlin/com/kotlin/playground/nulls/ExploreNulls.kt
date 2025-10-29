5f9def5b9a232fc73dbcb745e6c1004f7bc12337
JacekGos <jacekg93@gmail.com>
2025-10-29 11:35:46 +0100

    36.

--- src/main/kotlin/com/kotlin/playground/nulls/ExploreNulls.kt
+++ src/main/kotlin/com/kotlin/playground/nulls/ExploreNulls.kt
@@ -5,6 +5,14 @@ data class Movie(
     val name: String
 ) {}
 
+fun printName(name: String){
+    println("name is $name")
+}
+
+fun printName1(name: String?){
+    println("name1 is $name")
+}
+
 fun saveMovie(movie: Movie): Movie {
     return movie.copy(id = null)
 }
@@ -13,6 +21,14 @@ fun main() {
 
     var nameNullable: String? = null
     println("Value is: ${nameNullable?.length}")
+//    printName(nameNullable!!)
+    nameNullable?.run {
+        printName(this)
+    }
+
+
+    printName1(nameNullable)
+
     nameNullable = "Jacek"
     val length = nameNullable?.length?.toLong() ?: 0;
     println("length $length")
@@ -29,6 +45,6 @@ fun main() {
     val movie = Movie(null, "Nazwa")
     println(movie)
     val savedMovie: Movie = saveMovie(movie)
-    println(savedMovie.id!!)
+//    println(savedMovie.id!!)
     println(savedMovie)
 }
