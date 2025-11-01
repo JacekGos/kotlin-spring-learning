3b411c25637207b5e233156e405cac2707fea1f8
JacekGos <jacekg93@gmail.com>
2025-11-01 15:21:56 +0100

    .39

--- src/main/kotlin/com/kotlin/playground/collections/ExploreLambdas.kt
+++ src/main/kotlin/com/kotlin/playground/collections/ExploreLambdas.kt
@@ -1,5 +1,9 @@
 package com.kotlin.playground.collections
 
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+    return op(x, y)
+}
+
 fun main() {
 
     val addLambda = { x: Int -> x + x }
@@ -13,4 +17,9 @@ fun main() {
         result
     }
     println(multipleLambda(2, 4))
+
+    val calcResult = calculate(3, 3, multipleLambda)
+    val calcResult2 = calculate(3, 3) { x: Int, y: Int -> x * y }
+    println(calcResult)
+    println(calcResult2)
 }
